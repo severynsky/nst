73af86eb5303279ccfa57e0373a505677d8ec6c2
Severynsky <severynr@gmail.com>
2018-02-26 20:10:36 +0200

    contact page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import './assets/main.sass';
 import $ from 'jquery';
-console.log('hello!');
 
-$('.side-nav-handler').click(() => {
-  $('.offscreen-container').toggleClass('active-offscreen');
-  $('.side-nav-handler').toggleClass('active-button');
-  console.log('hello');
+const offscreenNav = $('.offscreen-container')
+const blackCurtain = $('.black-curtain');
+let navOpened = false;
+$('.side-nav-handler').click( () => {
+  if (!navOpened) {
+    $('.nav-item').each( (i, el) => {
+      $(el).delay((i++) * 180).fadeIn();
+    })
+    navOpened = true;
+  } else {
+    $('.nav-item').fadeOut(1);
+    navOpened = false;
+  }
+  offscreenNav.fadeIn(0)
+  offscreenNav.toggleClass('active-offscreen');
+  blackCurtain.toggleClass('show-curtain');
+  $('.side-nav-handler ul').toggleClass('active-button');
 })
+
+
+blackCurtain.click( () => {
+  offscreenNav.removeClass('active-offscreen');
+  blackCurtain.removeClass('show-curtain');
+  $('.side-nav-handler ul').removeClass('active-button');
+});
+
+$( 'body' ).mousemove( event => {
+  let msg = 'Handler for .mousemove() called at ';
+  const mX = Math.round((window.innerWidth/2 - event.pageX));
+  const mY = Math.round((window.innerHeight/2 - event.pageY));
+  $('.big-feather').css('-webkit-transform', 'translate3d(' + mX/100 + 'px, ' + mY/100 + 'px, 0)')
+});
